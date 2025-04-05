ac480c893d7bbfc7600bf709cf24f425614567e8
Taha <taharom12345@gmail.com>
2025-04-05 10:58:13 +0100

    =gg

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.index')
+@section('header')
+@include('layouts.starbootstrap.header')
+@endsection
+@section('title')
+taha
+@endsection
+@section('nav')
+@include('layouts.starbootstrap.nav')
+@endsection
+@section('fouter')
+@include('layouts.starbootstrap.fouter');
+@endsection
+
+
